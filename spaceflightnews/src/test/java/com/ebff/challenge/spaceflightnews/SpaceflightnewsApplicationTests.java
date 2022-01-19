2e181d3e17ffd2d8c524983a84ad1820c647e53e
Elias Barreto Ferreira Filho <ebff@fadeorg.onmicrosoft.com>
2022-01-18 21:14:09 -0300

    Create Project

---
+++ spaceflightnews/src/test/java/com/ebff/challenge/spaceflightnews/SpaceflightnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebff.challenge.spaceflightnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceflightnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
